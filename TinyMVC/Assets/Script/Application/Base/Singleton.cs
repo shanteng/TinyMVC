16540514efc4d7bcb5977cc9625010f35a13d90f
tinyshan <tinyshan@tencent.com>
2021-06-08 19:26:42 +0800

    修改ReadMe

---
+++ TinyMVC/Assets/Script/Application/Base/Singleton.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+/// <summary>
+/// 单例工厂,使用单例的可以直接继承他即可
+/// </summary>
+/// 
+
+public class Singleton<T> where T : new()
+{
+    private static readonly object sycObj = new object();
+    private static T t;
+    public static T Instance
+    {
+        get
+        {
+            if (t == null)
+            {
+                lock (sycObj)
+                {
+                    if (t == null)
+                    {
+                        t = new T();
+                    }
+                }
+            }
+            return t;
+        }
+    }
+
+
+    public Singleton()
+    {
+        this.init();
+    }
+
+    protected virtual void init()
+    {
+
+    }
+
+
+
+
+}
